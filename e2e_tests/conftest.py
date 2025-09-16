c6a12febad843440e212925a0b4c7879d3f26fe2
Maciej Skrzypkowski <mskr@gmx.com>
2025-09-16 12:28:00 +0200

    Improved checking for tx included and block mined
    test for precnfirming in handover window
    
    Signed-off-by: Maciej Skrzypkowski <mskr@gmx.com>

--- e2e_tests/conftest.py
+++ e2e_tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from web3 import Web3
+from web3.beacon import Beacon
 from eth_account import Account
 import os
 from dotenv import load_dotenv
@@ -20,3 +21,11 @@ def l2_client_node1():
 def l2_client_node2():
     w3 = Web3(Web3.HTTPProvider(os.getenv("L2_RPC_URL_NODE2")))
     return w3
+
+@pytest.fixture(scope="session")
+def beacon_client():
+    beacon_rpc_url = os.getenv("BEACON_RPC_URL")
+    if not beacon_rpc_url:
+        raise Exception("Environment variable BEACON_RPC_URL not set")
+
+    return Beacon(beacon_rpc_url)
