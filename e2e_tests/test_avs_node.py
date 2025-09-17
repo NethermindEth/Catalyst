e479bea217ea2c771174561b5497ed0e6e89d889
Maciej Skrzypkowski <mskr@gmx.com>
2025-09-17 08:41:57 +0200

    forced inclusion and hanover preconfirming tests (#600)
    * Improved checking for tx included and block mined
    
    test for precnfirming in handover window
    
    Signed-off-by: Maciej Skrzypkowski <mskr@gmx.com>
    
    * Forced inclusion test
    
    * .env.example updated
    
    * different private key for forced inclusion tx
    
    ---------
    
    Signed-off-by: Maciej Skrzypkowski <mskr@gmx.com>

--- e2e_tests/test_avs_node.py
+++ e2e_tests/test_avs_node.py
@@ -5,6 +5,8 @@ import os
 from dotenv import load_dotenv
 import sys
 from utils import *
+import subprocess
+import re
 
 load_dotenv()
 
@@ -13,7 +15,7 @@ if not l2_prefunded_priv_key:
     raise Exception("Environment variable TEST_L2_PREFUNDED_PRIVATE_KEY not set")
 
 
-def test_chain_ids(l1_client, l2_client_node1, l2_client_node2):
+def test_rpcs(l1_client, l2_client_node1, l2_client_node2, beacon_client):
     """Test to verify the chain IDs of L1 and L2 networks"""
     l1_chain_id = l1_client.eth.chain_id
     l2_chain_id_node1 = l2_client_node1.eth.chain_id
@@ -29,21 +31,21 @@ def test_chain_ids(l1_client, l2_client_node1, l2_client_node2):
     assert l1_chain_id != l2_chain_id_node1, "L1 and L2 should have different chain IDs"
     assert l2_chain_id_node1 == l2_chain_id_node2, "L2 nodes should have the same chain IDs"
 
+    spec = beacon_client.get_spec()
+    slots_per_epoch = int(spec['data']['SLOTS_PER_EPOCH'])
+    slot_duration = int(spec['data']['SECONDS_PER_SLOT'])
+    print(f"Slot Duration: {slot_duration}")
+    print(f"Slots Per Epoch: {slots_per_epoch}")
+    assert slot_duration > 0, "Slot duration should be greater than 0"
+    assert slots_per_epoch > 0, "Slots per epoch should be greater than 0"
+
 def test_preconfirm_transaction(l1_client, l2_client_node1):
     account = l2_client_node1.eth.account.from_key(l2_prefunded_priv_key)
     nonce = l2_client_node1.eth.get_transaction_count(account.address)
     l2_block_number = l2_client_node1.eth.block_number
 
-    send_transaction(nonce, account, '0.00005', l2_client_node1, l2_prefunded_priv_key)
-
-    wait_for_secs(4)
-
-    l2_block_number_after = l2_client_node1.eth.block_number
-
-    print(f"L2 Block Number: {l2_block_number}")
-    print(f"L2 Block Number After: {l2_block_number_after}")
-
-    assert l2_block_number_after > l2_block_number, "L2 block number should increase after sending a transaction"
+    tx_hash = send_transaction(nonce, account, '0.00005', l2_client_node1, l2_prefunded_priv_key)
+    assert wait_for_tx_to_be_included(l2_client_node1, tx_hash), "Transaction should be included in L2 Node 1"
 
 def test_p2p_preconfirmation(l2_client_node1, l2_client_node2):
     account = l2_client_node1.eth.account.from_key(l2_prefunded_priv_key)
@@ -52,7 +54,7 @@ def test_p2p_preconfirmation(l2_client_node1, l2_client_node2):
 
     send_transaction(nonce, account, '0.00006', l2_client_node1, l2_prefunded_priv_key)
 
-    wait_for_secs(4)
+    assert wait_for_new_block(l2_client_node2, l2_node_2_block_number), "L2 Node 2 should have a new block after sending a transaction"
 
     l2_node_2_block_number_after = l2_client_node2.eth.block_number
     node_1_block_hash = l2_client_node1.eth.get_block(l2_node_2_block_number_after).hash
@@ -61,6 +63,54 @@ def test_p2p_preconfirmation(l2_client_node1, l2_client_node2):
     print(f"L2 Node 2 Block Number: {l2_node_2_block_number}")
     print(f"L2 Node 2 Block Number After: {l2_node_2_block_number_after}")
 
-    assert l2_node_2_block_number_after > l2_node_2_block_number, "L2 Node 2 block number should increase after sending a transaction"
-
     assert node_2_block_hash == node_1_block_hash, "L2 Node 1 and L2 Node 2 should have the same block hash after sending a transaction"
+
+def test_handover_transaction(l2_client_node1, l2_client_node2, beacon_client):
+    wait_for_handover_window(beacon_client)
+
+    account = l2_client_node1.eth.account.from_key(l2_prefunded_priv_key)
+    nonce = l2_client_node1.eth.get_transaction_count(account.address)
+    l2_node_2_block_number = l2_client_node2.eth.block_number
+    print(f"L2 Node 2 Block Number: {l2_node_2_block_number}")
+
+    tx_hash = send_transaction(nonce, account, '0.00007', l2_client_node1, l2_prefunded_priv_key)
+    assert wait_for_tx_to_be_included(l2_client_node1, tx_hash), "Transaction should be included in L2 Node 1"
+    tx_hash = send_transaction(nonce+1, account, '0.00008', l2_client_node2, l2_prefunded_priv_key)
+    assert wait_for_tx_to_be_included(l2_client_node2, tx_hash), "Transaction should be included in L2 Node 2"
+
+def test_forced_inclusion(l2_client_node1):
+    """
+    This test runs the forced inclusion toolbox docker command and prints its output.
+    """
+    cmd = [
+        "docker", "run", "--network", "host", "--env-file", ".env", "--rm", "-it",
+        "nethswitchboard/taiko-forced-inclusion-toolbox", "send"
+    ]
+    print("Running forced inclusion toolbox docker command...")
+    try:
+        result = subprocess.run(cmd, capture_output=True, text=True, check=True)
+        print("Forced inclusion toolbox output:")
+        print(result.stdout)
+        if result.stderr:
+            print("Forced inclusion toolbox error output:")
+            print(result.stderr)
+
+        regex = r"hash=(0x[a-fA-F0-9]{64})"
+        match = re.search(regex, result.stdout)
+        assert match, "Could not find tx hash in forced inclusion toolbox output"
+        forced_inclusion_tx_hash = match.group(1)
+        print(f"Extracted forced inclusion tx hash: {forced_inclusion_tx_hash}")
+
+        # Spam 11 transactions to L2 Node to finish current batch
+        # and create new batch
+        spam_n_txs(l2_client_node1, l2_prefunded_priv_key, 11)
+
+        assert wait_for_tx_to_be_included(l2_client_node1, forced_inclusion_tx_hash), "Forced inclusion tx should be included in L2 Node 1"
+
+    except subprocess.CalledProcessError as e:
+        print("Error running forced inclusion toolbox docker command:")
+        print(e)
+        print("stdout:", e.stdout)
+        print("stderr:", e.stderr)
+        assert False, "Forced inclusion toolbox docker command failed"
+
--- e2e_tests/utils.py
+++ e2e_tests/utils.py
@@ -1,14 +1,22 @@
 import time
 
 def send_transaction(nonce : int, account, amount, eth_client, private_key):
+    base_fee = eth_client.eth.get_block('latest')['baseFeePerGas']
+    if base_fee < 25000000:
+        base_fee = 25000000
+    priority_fee = eth_client.eth.max_priority_fee
+    max_fee_per_gas = base_fee * 2 + priority_fee
     tx = {
         'nonce': nonce,
-        'to': "0x0000000000000000000000000000000000000001",
+        'to': '0x0000000000000000000000000000000000000001',
         'value': eth_client.to_wei(amount, 'ether'),
         'gas': 40000,
-        'gasPrice': eth_client.eth.gas_price,
-        'chainId': eth_client.eth.chain_id
+        'maxFeePerGas': max_fee_per_gas,
+        'maxPriorityFeePerGas': priority_fee,
+        'chainId': eth_client.eth.chain_id,
+        'type': 2  # EIP-1559 transaction type
     }
+
     print(f'RPC URL: {eth_client.provider.endpoint_uri}, Sending from: {account.address}')
     signed_tx = eth_client.eth.account.sign_transaction(tx, private_key)
     tx_hash = eth_client.eth.send_raw_transaction(signed_tx.raw_transaction)
@@ -20,3 +28,45 @@ def wait_for_secs(seconds):
         print(f'Waiting for {i:02d} seconds', end='\r')
         time.sleep(1)
     print('')
+
+def get_slot_in_epoch(beacon_client):
+    slots_per_epoch = int(beacon_client.get_spec()['data']['SLOTS_PER_EPOCH'])
+    current_slot = int(beacon_client.get_syncing()['data']['head_slot'])
+    return current_slot % slots_per_epoch
+
+def get_seconds_to_handover_window(beacon_client):
+    slot_in_epoch = get_slot_in_epoch(beacon_client)
+    if slot_in_epoch < 28:
+        return (28 - slot_in_epoch) * int(beacon_client.get_spec()['data']['SECONDS_PER_SLOT'])
+    else:
+        return 0
+
+def wait_for_tx_to_be_included(eth_client, tx_hash):
+    try:
+        eth_client.eth.wait_for_transaction_receipt(tx_hash, timeout=10)
+        return True
+    except Exception as e:
+        print(f"Error waiting for transaction to be included: {e}")
+        return False
+
+def wait_for_new_block(eth_client, initial_block_number):
+    for i in range(10):
+        if eth_client.eth.block_number > initial_block_number:
+            return True
+        time.sleep(1)
+    print(f"Error waited 10 seconds for new block, but block number did not increase")
+    return False
+
+def wait_for_handover_window(beacon_client):
+    seconds_to_handover_window = get_seconds_to_handover_window(beacon_client)
+    print(f"Seconds to handover window: {seconds_to_handover_window}")
+    wait_for_secs(seconds_to_handover_window)
+
+def spam_n_txs(eth_client, private_key, n):
+    account = eth_client.eth.account.from_key(private_key)
+    nonce = eth_client.eth.get_transaction_count(account.address)
+    last_tx_hash = None
+    for i in range(n):
+        last_tx_hash = send_transaction(nonce, account, '0.00009', eth_client, private_key)
+        wait_for_tx_to_be_included(eth_client, last_tx_hash)
+        nonce += 1
