064a99e048dec5c57b81fa7d553587415d397b5d
mikhailUshakoff <mikhail.ushakov@yahoo.com>
2025-09-22 11:00:52 +0200

    fix TaikoInbox abi

--- e2e_tests/taiko_inbox.py
+++ e2e_tests/taiko_inbox.py
@@ -1,9 +1,7 @@
-#import requests
 from web3 import Web3
 import os
 from dotenv import load_dotenv
-#import sys
-#from utils import *
+import json
 
 load_dotenv()
 
@@ -11,28 +9,8 @@ taiko_inbox_address = os.getenv("TAIKO_INBOX_ADDRESS")
 if not taiko_inbox_address:
     raise Exception("Environment variable TAIKO_INBOX_ADDRESS not set")
 
-abi = [
-    {
-        "inputs": [],
-        "name": "getStats2",
-        "outputs": [
-            {
-                "components": [
-                    {"internalType": "uint64", "name": "value0", "type": "uint64"},
-                    {"internalType": "uint64", "name": "value1", "type": "uint64"},
-                    {"internalType": "bool", "name": "value2", "type": "bool"},
-                    {"internalType": "uint56", "name": "value3", "type": "uint56"},
-                    {"internalType": "uint64", "name": "value4", "type": "uint64"},
-                ],
-                "internalType": "struct YourStructName",
-                "name": "",
-                "type": "tuple",
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function",
-    }
-]
+with open("../whitelist/src/l1/abi/ITaikoInbox.json") as f:
+    abi = json.load(f)
 
 def get_last_batch_id(l1_client):
     contract = l1_client.eth.contract(address=taiko_inbox_address, abi=abi)
