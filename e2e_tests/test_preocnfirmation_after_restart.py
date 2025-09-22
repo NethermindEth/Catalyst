0b1554a76fd95d84001ca74e4b93f52bf979f636
Maciej Skrzypkowski <mskr@gmx.com>
2025-09-22 18:04:30 +0200

    moved all env variables to the single class

--- e2e_tests/test_preocnfirmation_after_restart.py
+++ e2e_tests/test_preocnfirmation_after_restart.py
@@ -11,28 +11,6 @@ import time
 from eth_account import Account
 from taiko_inbox import get_last_batch_id
 
-load_dotenv()
-
-l2_prefunded_priv_key = os.getenv("TEST_L2_PREFUNDED_PRIVATE_KEY")
-if not l2_prefunded_priv_key:
-    raise Exception("Environment variable TEST_L2_PREFUNDED_PRIVATE_KEY not set")
-
-max_blocks_per_batch = int(os.getenv("MAX_BLOCKS_PER_BATCH"))
-if not max_blocks_per_batch:
-    raise Exception("Environment variable MAX_BLOCKS_PER_BATCH not set")
-
-preconf_heartbeat_ms = int(os.getenv("PRECONF_HEARTBEAT_MS"))
-if not max_blocks_per_batch:
-    raise Exception("Environment variable PRECONF_HEARTBEAT_MS not set")
-
-container_name_node1 = os.getenv("CONTAINER_NAME_NODE1")
-if not container_name_node1:
-    raise Exception("Environment variable CONTAINER_NAME_NODE1 not set")
-
-container_name_node2 = os.getenv("CONTAINER_NAME_NODE2")
-if not container_name_node2:
-    raise Exception("Environment variable CONTAINER_NAME_NODE2 not set")
-
 def restart_container(container_name):
     result = subprocess.run(["docker", "restart", container_name], capture_output=True, text=True, check=True)
     print("Restarted container:", container_name)
@@ -41,7 +19,7 @@ def restart_container(container_name):
         print(result.stderr)
         assert False, "Error restarting container"
 
-def test_preocnfirmation_after_restart(l1_client, beacon_client, l2_client_node1):
+def test_preocnfirmation_after_restart(l1_client, beacon_client, l2_client_node1, env_vars):
     """
     We restart the nodes and then produce 30 transactions every 2 L2 slots. We expect to receive 30 L2 blocks and 3 batches at the end
     """
@@ -52,19 +30,19 @@ def test_preocnfirmation_after_restart(l1_client, beacon_client, l2_client_node1
     print("Slot: ", slot)
     try:
         #restart nodes
-        restart_container(container_name_node1)
-        restart_container(container_name_node2)
+        restart_container(env_vars.container_name_node1)
+        restart_container(env_vars.container_name_node2)
         # wait for nodes warmup
         time.sleep(slot_duration_sec * 3)
         # get chain info
         block_number = l2_client_node1.eth.block_number
         print("Block number:", block_number)
-        batch_id = get_last_batch_id(l1_client)
+        batch_id = get_last_batch_id(l1_client, env_vars.taiko_inbox_address)
         # send transactions to create 3 batches
         # produce 1 L2 block every 2 L2 slots
-        delay = get_two_l2_slots_duration_sec(preconf_heartbeat_ms)
+        delay = get_two_l2_slots_duration_sec(env_vars.preconf_heartbeat_ms)
         print("delay", delay)
-        spam_n_txs_wait_only_for_the_last(l2_client_node1, l2_prefunded_priv_key, 3 * max_blocks_per_batch, delay)
+        spam_n_txs_wait_only_for_the_last(l2_client_node1, env_vars.l2_prefunded_priv_key, 3 * env_vars.max_blocks_per_batch, delay)
         # wait for transactions to be included on L1
         time.sleep(slot_duration_sec * 3)
         # verify
@@ -72,9 +50,9 @@ def test_preocnfirmation_after_restart(l1_client, beacon_client, l2_client_node1
         print("Slot: ", slot)
         new_block_number = l2_client_node1.eth.block_number
         print("New block number:", new_block_number)
-        new_batch_id = get_last_batch_id(l1_client)
+        new_batch_id = get_last_batch_id(l1_client, env_vars.taiko_inbox_address)
         print("New batch ID:", new_batch_id)
-        assert block_number + 3 * max_blocks_per_batch == new_block_number, "Invalid block number"
+        assert block_number + 3 * env_vars.max_blocks_per_batch == new_block_number, "Invalid block number"
         assert batch_id + 3 == new_batch_id, "Invalid batch ID"
     except subprocess.CalledProcessError as e:
         print("Error running test_three_consecutive_forced_inclusion")
