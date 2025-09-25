6789eba430dddf2c6fe52bc90515b838c0a5095c
mikhailUshakoff <mikhail.ushakov@yahoo.com>
2025-09-25 16:02:24 +0200

    feat: wait for geth sync before tests

--- e2e_tests/taiko_inbox.py
+++ e2e_tests/taiko_inbox.py
@@ -9,3 +9,10 @@ def get_last_batch_id(l1_client, taiko_inbox_address):
     result = contract.functions.getStats2().call()
     last_batch_id = result[0]
     return last_batch_id
+
+def get_last_block_id(l1_client, taiko_inbox_address):
+    batch_id = int(get_last_batch_id(l1_client, taiko_inbox_address)) - 1
+    contract = l1_client.eth.contract(address=taiko_inbox_address, abi=abi)
+    result = contract.functions.getBatch(batch_id).call()
+    last_block_id = result[1]
+    return last_block_id
