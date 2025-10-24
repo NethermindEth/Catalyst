bea9a0ce56c0391bf45690023d815e7b14a9a874
Maciej Skrzypkowski <mskr@gmx.com>
2025-10-24 12:36:36 +0200

    preparing event indexer usage

--- shasta/src/l2/mod.rs
+++ shasta/src/l2/mod.rs
@@ -1,2 +1,74 @@
 mod bindings;
 pub mod execution_layer;
+
+use crate::l2::execution_layer::L2ExecutionLayer;
+use alloy::primitives::B256;
+use anyhow::Error;
+use common::l2::taiko_driver::{
+    OperationType, TaikoDriver, TaikoDriverConfig,
+    models::{BuildPreconfBlockRequestBody, BuildPreconfBlockResponse, ExecutableData},
+};
+use common::shared::{
+    l2_block::L2Block, l2_slot_info::L2SlotInfo, l2_tx_lists::encode_and_compress,
+};
+use tracing::debug;
+use crate::event_indexer::EventIndexer;
+
+#[allow(clippy::too_many_arguments)]
+pub async fn advance_head_to_new_l2_block(
+    l2_block: L2Block,
+    anchor_origin_height: u64,
+    anchor_block_state_root: B256,
+    l2_slot_info: &L2SlotInfo,
+    end_of_sequencing: bool,
+    is_forced_inclusion: bool,
+    operation_type: OperationType,
+    driver: &TaikoDriver,
+    l2_execution_layer: &L2ExecutionLayer,
+    coinbase: &str,
+    event_indexer: &EventIndexer,
+) -> Result<Option<BuildPreconfBlockResponse>, Error> {
+    tracing::debug!(
+        "Submitting new L2 block to the Taiko driver with {} txs",
+        l2_block.prebuilt_tx_list.tx_list.len()
+    );
+
+    let base_fee_config = l2_execution_layer.get_base_fee_config();
+    let sharing_pctg = base_fee_config.sharingPctg;
+
+    event_indexer.;
+
+    let anchor_tx = l2_execution_layer
+        .construct_anchor_tx(
+            l2_slot_info,
+            anchor_origin_height,
+            anchor_block_state_root,
+            base_fee_config.clone(),
+        )
+        .await?;
+    let tx_list = std::iter::once(anchor_tx)
+        .chain(l2_block.prebuilt_tx_list.tx_list.into_iter())
+        .collect::<Vec<_>>();
+
+    let tx_list_bytes = encode_and_compress(&tx_list)?;
+    let extra_data = vec![sharing_pctg];
+
+    let executable_data = ExecutableData {
+        base_fee_per_gas: l2_slot_info.base_fee(),
+        block_number: l2_slot_info.parent_id() + 1,
+        extra_data: format!("0x{:0>64}", hex::encode(extra_data)),
+        fee_recipient: coinbase.to_string(),
+        gas_limit: 241_000_000u64,
+        parent_hash: format!("0x{}", hex::encode(l2_slot_info.parent_hash())),
+        timestamp: l2_block.timestamp_sec,
+        transactions: format!("0x{}", hex::encode(tx_list_bytes)),
+    };
+
+    let request_body = BuildPreconfBlockRequestBody {
+        executable_data,
+        end_of_sequencing,
+        is_forced_inclusion,
+    };
+
+    driver.preconf_blocks(request_body, operation_type).await
+}
--- shasta/src/lib.rs
+++ shasta/src/lib.rs
@@ -10,7 +10,6 @@ mod utils;
 mod l1;
 mod l2;
 
-use crate::utils::config::ShastaConfig;
 use anyhow::Error;
 use common::l1::{self as common_l1};
 use common::{config::Config, config::ConfigTrait, metrics::Metrics};
@@ -29,7 +28,7 @@ pub async fn create_shasta_node(
     info!("Creating Shasta node");
 
     let shasta_config = ShastaConfig::read_env_variables();
-    let _event_indexer = EventIndexer::new(
+    let event_indexer = EventIndexer::new(
         config
             .l1_rpc_urls
             .first()
@@ -55,11 +54,84 @@ pub async fn create_shasta_node(
         .await
         .map_err(|e| anyhow::anyhow!("Failed to create TaikoConfig: {}", e))?;
 
-    let _el = crate::l2::execution_layer::L2ExecutionLayer::new(taiko_config.clone())
-        .await
-        .map_err(|e| anyhow::anyhow!("Failed to create L2ExecutionLayer: {}", e))?;
+    let execution_layer_l2 =
+        crate::l2::execution_layer::L2ExecutionLayer::new(taiko_config.clone())
+            .await
+            .map_err(|e| anyhow::anyhow!("Failed to create L2ExecutionLayer: {}", e))?;
 
     let _node = Node::new(cancel_token).await?;
+
+    let signer = common::signer::create_signer(
+        config.web3signer_l2_url.clone(),
+        config.catalyst_node_ecdsa_private_key.clone(),
+        config.preconfer_address.clone(),
+    )
+    .await?;
+
+    use alloy::primitives::B256;
+    use common::l2::engine::L2Engine;
+    use common::l2::engine::L2EngineConfig;
+    use common::l2::taiko_driver::OperationType;
+    use common::l2::taiko_driver::TaikoDriver;
+    use common::l2::taiko_driver::TaikoDriverConfig;
+    use common::shared::{l2_block::L2Block, l2_slot_info::L2SlotInfo};
+    use std::time::Duration;
+
+    let l2_engine = L2Engine::new(L2EngineConfig::new(&config, signer.get_address())?)
+        .map_err(|e| anyhow::anyhow!("Failed to create L2Engine: {}", e))?;
+
+    let driver_config = TaikoDriverConfig {
+        driver_url: config.taiko_driver_url.clone(),
+        rpc_driver_preconf_timeout: config.rpc_driver_preconf_timeout,
+        rpc_driver_status_timeout: config.rpc_driver_status_timeout,
+        jwt_secret_bytes: common::utils::file_operations::read_jwt_secret(
+            &config.jwt_secret_file_path,
+        )?,
+        call_timeout: Duration::from_secs(config.preconf_heartbeat_ms / 2),
+    };
+    let driver = TaikoDriver::new(&driver_config, metrics).await?;
+
+    event_indexer.wait_historical_indexing_finished().await?;
+    loop {
+        let pending_tx_list = l2_engine
+            .get_pending_l2_tx_list(25000000, 0, 15000000)
+            .await?;
+
+        info!(
+            "Pending L2 tx list len: {:?}",
+            if let Some(pending_tx_list) = &pending_tx_list {
+                pending_tx_list.tx_list.len()
+            } else {
+                0
+            }
+        );
+
+        if let Some(pending_tx_list) = pending_tx_list {
+            let l2_block = L2Block::new_from(
+                pending_tx_list,
+                std::time::SystemTime::now() // temp solution
+                    .duration_since(std::time::UNIX_EPOCH)
+                    .unwrap()
+                    .as_secs(),
+            );
+
+            l2::advance_head_to_new_l2_block(
+                l2_block,
+                0,
+                B256::ZERO,
+                &L2SlotInfo::new(0, 0, 0, B256::ZERO, 0),
+                false,
+                false,
+                OperationType::Preconfirm,
+                &driver,
+                &execution_layer_l2,
+                &event_indexer,
+            )
+            .await?;
+        }
+
+        tokio::time::sleep(Duration::from_secs(2)).await;
+    }
 
     Ok(())
 }
