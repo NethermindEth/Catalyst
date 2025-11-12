03d4fe14cd169f38422352c18ee80b7173ac982d
mikhailUshakoff <mikhail.ushakov@yahoo.com>
2025-11-12 11:20:40 +0100

    fix: better calculation of last L2 block on L1

--- shasta/src/node/l2_head_provider.rs
+++ shasta/src/node/l2_head_provider.rs
@@ -2,6 +2,7 @@ use crate::{l1::execution_layer::ExecutionLayer, l2::taiko::Taiko};
 use anyhow::Error;
 use common::l1::ethereum_l1::EthereumL1;
 use std::sync::Arc;
+use tracing::debug;
 
 pub async fn get_l2_height_from_l1(
     ethereum_l1: Arc<EthereumL1<ExecutionLayer>>,
@@ -11,8 +12,18 @@ pub async fn get_l2_height_from_l1(
         .execution_layer
         .get_proposal_id_from_indexer()
         .await?;
-    taiko
+
+    if proposal_id == 0 {
+        // TODO retrieve it from Pacaya inbox
+        return Ok(0);
+    }
+
+    let result = taiko
         .l2_execution_layer()
         .get_last_block_by_proposal(proposal_id)
-        .await
+        .await?;
+
+    debug!("get_l2_height_from_l1: proposal id: {}, last block by proposal: {}", proposal_id, result);
+
+    Ok(result)
 }
