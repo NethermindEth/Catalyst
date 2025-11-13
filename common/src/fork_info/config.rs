c81c2106629a879b250aa19fb3c78d735097a202
Maciej Skrzypkowski <mskr@gmx.com>
2025-11-13 17:03:16 +0100

    clippy

--- common/src/fork_info/config.rs
+++ common/src/fork_info/config.rs
@@ -13,10 +13,7 @@ impl From<&Config> for ForkInfoConfig {
     fn from(config: &Config) -> Self {
         Self {
             initial_fork: config.initial_fork.clone(),
-            fork_switch_timestamp: match config.fork_switch_timestamp {
-                Some(timestamp) => Some(Duration::from_secs(timestamp)),
-                None => None,
-            },
+            fork_switch_timestamp: config.fork_switch_timestamp.map(Duration::from_secs),
             fork_switch_l2_height: config.fork_switch_l2_height,
             fork_switch_transition_period: Duration::from_secs(
                 config.fork_switch_transition_period_sec,
