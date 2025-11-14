02094eed105e31fd26e29ade2fdc4683a2e93f13
mikhailUshakoff <mikhail.ushakov@yahoo.com>
2025-11-14 15:08:18 +0100

    fix get_l2_height_from_l1

--- shasta/src/node/l2_height_from_l1.rs
+++ shasta/src/node/l2_height_from_l1.rs
@@ -7,14 +7,20 @@ pub async fn get_l2_height_from_l1(
     ethereum_l1: Arc<EthereumL1<ExecutionLayer>>,
     taiko: Arc<Taiko>,
 ) -> Result<u64, Error> {
-    let proposal_id = ethereum_l1
-        .execution_layer
-        .get_proposal_id_from_indexer()
-        .await?;
+    match taiko.l2_execution_layer().get_head_l1_origin().await {
+        Ok(height) => Ok(height),
+        Err(err) => {
+            // On error, check proposal_id
+            let proposal_id = ethereum_l1
+                .execution_layer
+                .get_proposal_id_from_indexer()
+                .await?;
 
-    if proposal_id == 0 {
-        return Ok(0);
+            if proposal_id == 0 {
+                Ok(0)
+            } else {
+                Err(err)
+            }
+        }
     }
-
-    taiko.l2_execution_layer().get_head_l1_origin().await
 }
