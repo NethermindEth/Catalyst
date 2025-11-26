19e758c680accd2eaffca5e09868c557fc390b5b
mikhailUshakoff <mikhail.ushakov@yahoo.com>
2025-11-26 10:34:27 +0100

    bump version

--- shasta/src/l1/proposal_tx_builder.rs
+++ shasta/src/l1/proposal_tx_builder.rs
@@ -123,10 +123,6 @@ impl ProposalTxBuilder {
                 coinbase: l2_block.coinbase,
                 anchor_block_number: l2_block.anchor_block_number,
                 gas_limit: l2_block.gas_limit,
-                //gas_limit: 15_000_000, // TODO calculate it and store inside l2_block
-                // 0 - does not work for now
-                /* Use 0 for gas limit as it will be set as its parent's gas
-                 * limit during derivation. */
                 transactions: l2_block
                     .prebuilt_tx_list
                     .tx_list
