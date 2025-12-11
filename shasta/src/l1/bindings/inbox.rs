d6da55104ded918d4aa1c78ff03b577e17196eec
Maciej Skrzypkowski <mskr@gmx.com>
2025-12-11 13:22:47 +0100

    Revert "taiko inbox bindings used from the taiko-client-rs"
    This reverts commit af631d088d36bdf90153cab3fbc65294588da635.

---
+++ shasta/src/l1/bindings/inbox.rs
@@ -0,0 +1,93 @@
+use alloy::sol;
+
+sol!(
+#[sol(rpc)]
+ contract Inbox {
+    struct Config {
+        /// @notice The codec used for encoding and hashing
+        address codec;
+        /// @notice The token used for bonds
+        address bondToken;
+        /// @notice The signal service contract address
+        address signalService;
+        /// @notice The proof verifier contract
+        address proofVerifier;
+        /// @notice The proposer checker contract
+        address proposerChecker;
+        /// @notice The proving window in seconds
+        uint48 provingWindow;
+        /// @notice The extended proving window in seconds
+        uint48 extendedProvingWindow;
+        /// @notice The ring buffer size for storing proposal hashes
+        uint256 ringBufferSize;
+        /// @notice The percentage of basefee paid to coinbase
+        uint8 basefeeSharingPctg;
+        /// @notice The minimum number of forced inclusions that the proposer is forced to process
+        /// if they are due
+        uint256 minForcedInclusionCount;
+        /// @notice The delay for forced inclusions measured in seconds
+        uint16 forcedInclusionDelay;
+        /// @notice The base fee for forced inclusions in Gwei used in dynamic fee calculation
+        uint64 forcedInclusionFeeInGwei;
+        /// @notice Queue size at which the fee doubles
+        uint64 forcedInclusionFeeDoubleThreshold;
+        /// @notice The minimum delay between checkpoints in seconds
+        /// @dev Must be less than or equal to finalization grace period
+        uint16 minCheckpointDelay;
+        /// @notice The multiplier to determine when a forced inclusion is too old so that proposing
+        /// becomes permissionless
+        uint8 permissionlessInclusionMultiplier;
+    }
+
+    struct BlobSlice {
+        /// @notice The blobs containing the proposal's content.
+        bytes32[] blobHashes;
+        /// @notice The byte offset of the proposal's content in the containing blobs.
+        uint24 offset;
+        /// @notice The timestamp when the frame was created.
+        uint48 timestamp;
+    }
+
+    struct CoreState {
+        /// @notice The next proposal ID to be assigned.
+        uint48 nextProposalId;
+        /// @notice The last L1 block ID where a proposal was made.
+        uint48 lastProposalBlockId;
+        /// @notice The ID of the last finalized proposal.
+        uint48 lastFinalizedProposalId;
+        /// @notice The timestamp when the last proposal was finalized.
+        uint48 lastFinalizedTimestamp;
+        /// @notice The timestamp when the last checkpoint was saved.
+        /// @dev In genesis block, this is set to 0 to allow the first checkpoint to be saved.
+        uint48 lastCheckpointTimestamp;
+        /// @notice The hash of the last finalized transition.
+        bytes32 lastFinalizedTransitionHash;
+    }
+
+    struct ForcedInclusion {
+        uint64 feeInGwei;
+        BlobSlice blobSlice;
+    }
+
+    uint48 public activationTimestamp;
+
+    function getForcedInclusionState()
+        external
+        view
+        returns (uint48 head_, uint48 tail_, uint48 lastProcessedAt_);
+
+    function getForcedInclusions(
+            uint48 _start,
+            uint48 _maxCount
+        )
+            external
+            view
+            returns (ForcedInclusion[] memory inclusions_);
+
+    function propose(bytes calldata _lookahead, bytes calldata _data) external;
+
+    function getConfig() external view returns (Config memory config_);
+
+    function getState() external view returns (CoreState memory state_);
+}
+);
--- shasta/src/l1/bindings/mod.rs
+++ shasta/src/l1/bindings/mod.rs
@@ -1,3 +1,5 @@
+mod inbox;
 mod preconf_whitelist;
 
+pub use inbox::Inbox;
 pub use preconf_whitelist::PreconfWhitelist;
