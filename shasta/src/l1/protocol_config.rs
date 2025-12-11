9525e3b250505ed0e80e7a930a62e7703a20112d
Maciej Skrzypkowski <mskr@gmx.com>
2025-12-11 09:06:21 +0100

    Handling Proposed event for shasta (#763)
    * Handling Proposed event for shasta
    
    * remove comment

--- shasta/src/l1/protocol_config.rs
+++ shasta/src/l1/protocol_config.rs
@@ -1,4 +1,5 @@
 use super::bindings::Inbox::Config;
+use alloy::primitives::Address;
 
 #[derive(Clone, Default)]
 pub struct ProtocolConfig {
@@ -6,6 +7,7 @@ pub struct ProtocolConfig {
     // TODO initialize these values correctly
     _min_anchor_offset: u64,
     max_anchor_offset: u64,
+    codec_address: Address,
 }
 
 impl ProtocolConfig {
@@ -14,6 +16,7 @@ impl ProtocolConfig {
             basefee_sharing_pctg: shasta_config.basefeeSharingPctg,
             _min_anchor_offset: 2, // https://github.com/taikoxyz/taiko-mono/blob/main/packages/protocol/docs/Derivation.md#constants
             max_anchor_offset: 100, // 128 by document
+            codec_address: shasta_config.codec,
         }
     }
 
@@ -28,5 +31,9 @@ impl ProtocolConfig {
 
     pub fn get_max_anchor_height_offset(&self) -> u64 {
         self.max_anchor_offset
+    }
+
+    pub fn get_codec_address(&self) -> Address {
+        self.codec_address
     }
 }
