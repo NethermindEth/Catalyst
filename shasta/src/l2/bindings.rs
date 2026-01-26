9098d267954af25b54f891ee57dafbdfcab7709c
Maciej Skrzypkowski <mskr@gmx.com>
2026-01-26 17:09:50 +0100

    removed unneeded bindings

--- shasta/src/l2/bindings.rs
+++ shasta/src/l2/bindings.rs
@@ -5,13 +5,6 @@ use alloy::sol;
 sol!(
     #[allow(missing_docs)]
     #[sol(rpc)]
-    BondManager,
-    "src/l2/abi/BondManager.json"
-);
-
-sol!(
-    #[allow(missing_docs)]
-    #[sol(rpc)]
     Bridge,
     "src/l2/abi/Bridge.json"
 );
