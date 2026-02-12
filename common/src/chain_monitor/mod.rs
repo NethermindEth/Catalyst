b4b762a663fc367bec5799c370a804f69f6e7914
mikhailUshakoff <75278099+mikhailUshakoff@users.noreply.github.com>
2026-02-12 14:49:23 +0100

    feat: recover FI that we produced (#861)
    * feat: recover FI that we produced
    
    * chore: fix log message
    
    * Update common/src/chain_monitor/mod.rs
    
    Co-authored-by: Maciej Skrzypkowski <mskr@gmx.com>
    
    * Update common/src/chain_monitor/mod.rs
    
    Co-authored-by: Maciej Skrzypkowski <mskr@gmx.com>
    
    ---------
    
    Co-authored-by: Maciej Skrzypkowski <mskr@gmx.com>

--- common/src/chain_monitor/mod.rs
+++ common/src/chain_monitor/mod.rs
@@ -143,9 +143,9 @@ where
                                 None => false,
                             };
                             if reorg_expected {
-                                tracing::debug!("Geth reorg detected: Received L2 block with expected number. Expected: block_id {} hash {}", status.height, status.hash);
+                                tracing::debug!("Geth reorg detected: Received L2 block with expected number. Expected: block id {} parent hash {}", status.height+1, status.hash);
                             } else {
-                                tracing::warn!("⛔ Geth reorg detected: Received L2 block with unexpected number. Expected: block_id {} hash {}", status.height, status.hash);
+                                tracing::warn!("⛔ Geth reorg detected: Received L2 block with unexpected number. Expected: block id {} parent hash {}", status.height+1, status.hash);
                             }
                         }
 
