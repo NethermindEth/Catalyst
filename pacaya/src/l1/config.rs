5f8cdb59aa454b8101c624b1feb1f6786fa5ac83
Maciej Skrzypkowski <mskr@gmx.com>
2026-04-09 14:35:24 +0200

    Removing pacaya node

--- pacaya/src/l1/config.rs
+++ pacaya/src/l1/config.rs
@@ -1,4 +1,3 @@
-use crate::utils::config::{L1ContractAddresses, PacayaConfig};
 use alloy::primitives::Address;
 use tokio::sync::OnceCell;
 
@@ -12,31 +11,6 @@ pub struct ContractAddresses {
     pub forced_inclusion_store: Address,
 }
 
-impl TryFrom<L1ContractAddresses> for ContractAddresses {
-    type Error = anyhow::Error;
-
-    fn try_from(l1_contract_addresses: L1ContractAddresses) -> Result<Self, Self::Error> {
-        Ok(ContractAddresses {
-            taiko_inbox: l1_contract_addresses.taiko_inbox,
-            taiko_token: OnceCell::new(),
-            preconf_whitelist: l1_contract_addresses.preconf_whitelist,
-            preconf_router: l1_contract_addresses.preconf_router,
-            taiko_wrapper: l1_contract_addresses.taiko_wrapper,
-            forced_inclusion_store: l1_contract_addresses.forced_inclusion_store,
-        })
-    }
-}
-
 pub struct EthereumL1Config {
     pub contract_addresses: ContractAddresses,
-}
-
-impl TryFrom<PacayaConfig> for EthereumL1Config {
-    type Error = anyhow::Error;
-
-    fn try_from(config: PacayaConfig) -> Result<Self, Self::Error> {
-        Ok(EthereumL1Config {
-            contract_addresses: ContractAddresses::try_from(config.contract_addresses)?,
-        })
-    }
 }
--- pacaya/src/node/batch_manager/config.rs
+++
@@ -1,6 +0,0 @@
-use super::batch::Batch;
-use crate::l1::bindings::BatchParams;
-use std::collections::VecDeque;
-
-pub type ForcedInclusionBatch = Option<BatchParams>;
-pub type BatchesToSend = VecDeque<(ForcedInclusionBatch, Batch)>;
